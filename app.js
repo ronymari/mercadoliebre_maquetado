c5934e254c2e9894686edf02e470d1cdf0667ac6
Rony Mari <rony.s.mari@hotmail.com>
2023-11-12 17:54:55 -0300

    forms

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express= require('express');
-const path=require('path');
-const app= express();
-const PORT= 3030;
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3030;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 
-app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views','home.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
-
-
-
-app.listen(PORT,()=> console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`));
